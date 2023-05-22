584b6d9b40e1cd5e0d3cd698832920d50c745f9e
Simone PriMaL Maccarone <80923893+SimoneMaccaarone@users.noreply.github.com>
2023-05-22 17:44:21 +0200

    import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,9 +9,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
-  import {MatButtonModule} from '@angular/material/button';
+import {MatButtonModule} from '@angular/material/button';
 import {MatMenuModule} from '@angular/material/menu';
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import {MatSelectModule} from '@angular/material/select';
 
 
 @NgModule({
@@ -28,7 +29,8 @@ import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
     MatIconModule,
     HttpClientInMemoryWebApiModule,
     MatButtonModule,
-    MatMenuModule
+    MatMenuModule,
+    MatSelectModule,
 
 
 
