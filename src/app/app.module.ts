c34e825e9a1477f31425470a0ebdcb80dbc51510
Simone PriMaL Maccarone <80923893+SimoneMaccaarone@users.noreply.github.com>
2023-05-24 14:42:47 +0200

    pc portatitle

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './components/home-page/home-page.component';
+import { CardComponent } from './components/card/card.component';
+
+
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
@@ -14,7 +17,8 @@ import {MatMenuModule} from '@angular/material/menu';
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 import {MatSelectModule} from '@angular/material/select';
 import {MatCardModule} from '@angular/material/card';
-import { CardComponent } from './components/card/card.component';
+import { FormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
@@ -34,7 +38,8 @@ import { CardComponent } from './components/card/card.component';
     MatButtonModule,
     MatMenuModule,
     MatSelectModule,
-    MatCardModule
+    MatCardModule,
+    FormsModule
 
 
 
